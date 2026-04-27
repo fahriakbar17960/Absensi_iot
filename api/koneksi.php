ec40938210615cec5f20c2aee0cbd964b0743c61
fahriakbar17960 <fahri.akbar17960@smk.belajar.id>
2026-04-27 12:13:24 +0700

    koneksi.php

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,21 +1,13 @@
 <?php
-// Data dari Clever Cloud
 $host = "bcgqnmv1lwnzerqzgdft-mysql.services.clever-cloud.com";
 $user = "umqkft2nhxc4nnyo";
 $pass = "z8KBUIqMUD7sxNEtZyfJ";
 $db   = "bcgqnmv1lwnzerqzgdft";
 
-// Membuat koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
-
-// Menyamakan variabel agar tidak error di file lain
 $conn = $koneksi; 
 
-// Cek koneksi
 if (!$koneksi) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-// Set timezone agar waktu sesuai
-date_default_timezone_set('Asia/Jakarta');
 ?>
